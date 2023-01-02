960441c15322eab39df8d1cb37177f586f08f3c0
Iftee97 <tawsifhaque97@gmail.com>
2023-01-02 18:18:56 +0600

    added crud functionality with persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import BookCreate from "./components/BookCreate"
 import BookList from "./components/BookList"
 
 export default function App() {
   const [books, setBooks] = useState([])
 
-  const createBook = (title) => {
-    console.log(`Book title: ${title}`)
-    setBooks([
-      ...books,
-      {
-        id: Date.now(),
-        title
-      }
-    ])
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('http://localhost:3001/books')
+      setBooks(data)
+    }
+    fetchData()
+  }, [])
+
+  const createBook = async (title) => {
+    const { data } = await axios.post('http://localhost:3001/books', {
+      title
+    })
+    setBooks([...books, data])
   }
 
-  const deleteBook = (id) => {
+  const deleteBook = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
     setBooks(books.filter((book) => book.id !== id))
   }
 
-  const editBook = (id, title) => {
-    setBooks(books.map((book) => {
-      if (book.id === id) {
-        return {
-          ...book,
-          title
-        }
-      }
-      return book
-    }))
+  const editBook = async (id, title) => {
+    const { data } = await axios.put(`http://localhost:3001/books/${id}`, {
+      title
+    })
+    setBooks(books.map((book) => (book.id === id ? data : book)))
   }
 
   return (
@@ -44,5 +45,3 @@ export default function App() {
     </div>
   )
 }
-
-// https://state-updates.vercel.app/
