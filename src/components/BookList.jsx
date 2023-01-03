531bde04edaff66542db0e1ab0ef8b9d597396fa
Iftee97 <tawsifhaque97@gmail.com>
2023-01-03 13:53:01 +0600

    initial refactor

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,16 +2,13 @@ import { useContext } from "react"
 import { BooksContext } from "../context/BooksContext"
 import BookShow from "./BookShow"
 
-export default function BookList({ books, deleteBook, editBook }) {
+export default function BookList() {
+  const { books } = useContext(BooksContext)
+
   return (
     <div className="book-list">
       {books.map((book) => (
-        <BookShow
-          book={book}
-          key={book.id}
-          deleteBook={deleteBook}
-          editBook={editBook}
-        />
+        <BookShow book={book} key={book.id} />
       ))}
     </div>
   )
